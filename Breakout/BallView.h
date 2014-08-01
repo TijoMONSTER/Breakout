ebc7321ff918bb4b0cbd4ab73c9bcca61737e47e
Iván Mervich <imervich@gmail.com>
2014-07-31 21:28:14 -0500

    Added BallView

---
+++ Breakout/BallView.h
@@ -0,0 +1,13 @@
+//
+//  BallView.h
+//  Breakout
+//
+//  Created by Iván Mervich on 7/31/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BallView : UIView
+
+@end
---
+++ Breakout/BallView.m
@@ -0,0 +1,13 @@
+//
+//  BallView.m
+//  Breakout
+//
+//  Created by Iván Mervich on 7/31/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "BallView.h"
+
+@implementation BallView
+
+@end
